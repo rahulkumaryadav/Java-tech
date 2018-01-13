a373147b97d7defc422f971e032e846a3badd318
Rahul kumar <rahulkumar96369@gmail.com>
2018-01-13 19:31:14 +0530

    object1 example is commited

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,23 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Java Object Based Elements");
+        System.out.println("welcome to java Object--->Please refer the following link");
+
     }
-    /********------------------------Syllabus----------------------------------------------------
-     *
-     3.Java Object Based Elements
-        Class
-        Object
-        Message, State, Behaviour and Structure.
-        Responsibility Driven Programming
-        Initializer, Constructor, Finalizer
-        Static and Instance fields
-        Overloading
-        Cloning
-        Object Composition
-        Linking and Embedding
-        Garbage Collector
-        Heap Manager
-        Enumeration
-     ********/
+    /*
+https://dzone.com/articles/5-different-ways-to-create-objects-in-java-with-ex
+
+    5 Different Ways to Create Objects in Java
+    Using new keyword} → constructor gets called
+    Using newInstance() method of Class class} → constructor gets called
+    Using newInstance() method of Constructor class} → constructor gets called
+    Using clone() method} → no constructor call
+    Using deserialization} → no constructor call
+     */
 }
