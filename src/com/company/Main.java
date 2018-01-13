758b692b4b64fa05117caf5ea5c31b087276e7cd
Rahul kumar <rahulkumar96369@gmail.com>
2018-01-13 17:36:39 +0530

    basic concept of class is commited

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,32 @@
 package com.company;
+/*
+A class is a group of objects which have common properties.
+It is a template or blueprint from which objects are created.
+It is a logical entity. It can't be physical.
+A class in Java can contain:
+    fields
+    methods
+    constructors
+    blocks
+    nested class and interface
+Syntax to declare a class:
+    class <class_name>{
+        field;
+        method;
+    }
+
+NOTE:To study class we should know following concepts
+    Object in Java
+    Instace Variable in Java
+    Method in Java
+    Example of Object and class that maintains the records of student
+    Annonymous Object
+
+ */
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Java Object Based Elements");
+        System.out.println("Welcome to class in java");
     }
-    /********------------------------Syllabus----------------------------------------------------
-     *
-     3.Java Object Based Elements
-        Class
-        Object
-        Message, State, Behaviour and Structure.
-        Responsibility Driven Programming
-        Initializer, Constructor, Finalizer
-        Static and Instance fields
-        Overloading
-        Cloning
-        Object Composition
-        Linking and Embedding
-        Garbage Collector
-        Heap Manager
-        Enumeration
-     ********/
 }
